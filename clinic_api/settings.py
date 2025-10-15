9684884880191539d9d4ac780c7b388a0a02dd56
wondavibes <amosayemoro2001@gmail.com>
2025-10-15 22:07:32 +0100

    edited functions

--- clinic_api/settings.py
+++ clinic_api/settings.py
@@ -63,6 +63,16 @@ if not DEBUG:
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
     ]
 
+if DEBUG:
+    MIDDLEWARE = [
+        "django.middleware.security.SecurityMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    ]
 ROOT_URLCONF = "clinic_api.urls"
 
 TEMPLATES = [
